ec097cad0581930cae1e9bc51c281d4c4dc70cec
Lmlilas Ibrahim <spedibra@gmail.com>
2025-03-24 15:36:15 +0100

    Update Blog.blade.php

--- resources/views/visitors/Blog.blade.php
+++ resources/views/visitors/Blog.blade.php
@@ -49,14 +49,14 @@
     #home{
         cursor: none;
     }
-    
+
 
 </style>
 
 <div class="relative h-screen overflow-hidden " id="home">
     <!-- Background Video -->
     <video autoplay muted loop class="absolute w-full h-full object-cover">
-        <source src="{{ asset('videos/HACK&GO.webm') }}" type="video/webm">
+        <source src="https://rr4---sn-h5qzened.googlevideo.com/videoplayback?expire=1742837443&ei=Y0LhZ-XRHKjRp-oPz5eQ8AE&ip=176.1.223.37&id=o-AK9HdH2sILHYtIbn-VX6VQfGMuAm_1h84N-CHxRqmnFa&itag=313&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C271%2C278%2C313&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&bui=AccgBcNfq5uIHkzNhmyzaqboiT8oyr2ZYFKpXvof2n3f_hn3vC4evyoVW34Yx28cNgnLafLnUtE234VM&spc=_S3wKsocd6Y_5fuRigNzwxlVMR0AlGCfHF31f7oVakyMDxQlQQ&vprv=1&svpuc=1&mime=video%2Fwebm&ns=5BMGaCJBcpYKLhmS5cR-AlcQ&rqh=1&gir=yes&clen=114830085&dur=111.193&lmt=1695244864148387&keepalive=yes&fexp=24350590,24350737,24350778,24350827,24350961,24351147,24351173,24351177,24351283,24351353,24351394,24351396,24351398,24351468,51355912&c=WEB&sefc=1&txp=5319224&n=wQjjVoIaMNuxuQ&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRAIgRWTrY_KK_J3Sa3tjSnfQSyFItu9KzHFnBxag_0PoA_UCIHQCPAg6bR21enA2jIwkVJJwQet9Gvqdg2q_pyCG_yMH&pot=MnStG18rFtRPF60nBkVFVmfGH5kMY_LJmF7Jq3CGwH3VReIUtjBhPvLKJQGYmlBofCex5D32jyV7t3MTe-94vXx1Tq2z0jAzAcqOak9rtRkblwHeM3QLDdekL9BlWg49e7y6YyBTWcM-UMTPIz6QgOFLZysstw==&rm=sn-uxax4vopj5qx-q0n67l,sn-4g5erd7s&rrc=79,104&req_id=fc14140cee8da3ee&rms=rdu,au&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&met=1742815862,&mh=oS&mip=197.230.119.22&mm=29&mn=sn-h5qzened&ms=rdu&mt=1742815754&mv=m&mvi=4&pl=25&lsparams=ipbypass,met,mh,mip,mm,mn,ms,mv,mvi,pl,rms&lsig=AFVRHeAwRAIgfMZqKCvrgNygYyOn0CsvdhR3_xoMTznipYhnaEsDHmACIFrpVZ0qUwLV3lvlD2zZtSKCTzKM2N7Mz8MHnvydPWXR" >
     </video>
 
     <!-- Overlay -->
